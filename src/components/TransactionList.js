502be413b8608d3260fbf7da0da0c80b8e93a6ae
Anu Shree <anushree104120@gmail.com>
2024-02-26 09:08:50 +0530

    first

---
+++ src/components/TransactionList.js
@@ -0,0 +1,22 @@
+import { useContext } from "react"
+import { GlobalContext } from "../context/GlobalState"
+import Transaction from "./Transaction";
+
+
+const TransactionList = () => {
+  const { transactions } = useContext(GlobalContext);
+
+  
+  return (
+    <>
+      <h3>Transaction History</h3>
+      <ul id="list" className="list">
+        {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
+      </ul>
+
+    </>
+  )
+}
+
+export default TransactionList
+
---
+++ src/context/GlobalState.js
@@ -0,0 +1,51 @@
+import PropTypes from 'prop-types';
+import { createContext, useReducer } from "react";
+import AppReducer from './AppReducer'
+
+// initial state
+const initialState = {
+    transactions: []
+
+}
+
+// Create context
+export const GlobalContext = createContext(initialState)
+
+// Provider component to wrap all component
+export const GlobalProvider = ({ children }) =>{
+ const [state, dispatch] = useReducer(AppReducer, initialState);
+
+ // Action to delete transaction
+  const deleteTransaction = (id) =>{
+    dispatch({
+        type: 'DELETE_TRANSACTION',
+        payload: id
+    })
+  }
+
+  // Action to Add transaction
+  const addTransaction = (transactions) =>{
+    dispatch({
+        type: 'ADD_TRANSACTION',
+        payload: transactions
+    })
+  }
+
+ return (
+
+ <GlobalContext.Provider value={{
+    transactions: state.transactions,
+    deleteTransaction,
+    addTransaction
+}}>
+    
+    {children}</GlobalContext.Provider>
+ 
+ );
+}
+
+
+GlobalProvider.propTypes = {
+    children: PropTypes.node.isRequired
+  };
+  export default GlobalProvider
